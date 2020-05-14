3d9e403975e804e099f631abe2a15df4f6de3b30
garrett krage <garrett.krage@gmail.com>
2020-05-14 09:07:47 -0600

    app.js update.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import Main from './components/pages/Main';
 import Careers from './components/pages/Careers'
 import Contact from './components/pages/Contact'
