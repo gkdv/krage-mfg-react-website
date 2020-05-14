ddc4b21afbf25c190c49c8b5e338ee09b172804a
garrett krage <garrett.krage@gmail.com>
2020-05-14 12:25:49 -0600

    trying to get app to deploy

---
+++ build/precache-manifest.e68be5c9a6a1720292c7b45f865e9599.js
@@ -0,0 +1,414 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98cfaf98b5f79e85f4e768451aad205b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75c26244278682b57cb1",
+    "url": "/static/css/main.e96938b8.chunk.css"
+  },
+  {
+    "revision": "aa0a73310fe5626c2829",
+    "url": "/static/js/2.f5008753.chunk.js"
+  },
+  {
+    "revision": "bdfa2c3e6f6699725955bbe62a396cbc",
+    "url": "/static/js/2.f5008753.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75c26244278682b57cb1",
+    "url": "/static/js/main.2acd76b2.chunk.js"
+  },
+  {
+    "revision": "34a9b4cbd6126fe5b5a8",
+    "url": "/static/js/runtime-main.dc72e2d1.js"
+  },
+  {
+    "revision": "621e5a2620e7032aa8fac57edcb0f28d",
+    "url": "/static/media/Krage_Logo.621e5a26.png"
+  },
+  {
+    "revision": "19b90fb56cc3b3e4f5c03606ce12fb13",
+    "url": "/static/media/MurataTurretPress544x408.19b90fb5.JPG"
+  },
+  {
+    "revision": "290ed7a8ba20ac8d364a68c044c4e806",
+    "url": "/static/media/aboutFloorPhoto.290ed7a8.JPG"
+  },
+  {
+    "revision": "8143034bd3df946552da354003349b0c",
+    "url": "/static/media/airCondition722x428.8143034b.jpg"
+  },
+  {
+    "revision": "7b7b01ce5f2b30ce0c755978cacae976",
+    "url": "/static/media/architecture722x481.7b7b01ce.jpg"
+  },
+  {
+    "revision": "50a55a3bb0c3804e55b1f330441e9cc2",
+    "url": "/static/media/assemblyCapabilities.50a55a3b.JPG"
+  },
+  {
+    "revision": "85d2e07135740b76cf913e3385b9435d",
+    "url": "/static/media/bystronic_press1_290x218.85d2e071.JPG"
+  },
+  {
+    "revision": "de03f69e6ce22a969eecdf149cf6c256",
+    "url": "/static/media/bystronic_press1_400x300.de03f69e.JPG"
+  },
+  {
+    "revision": "ceea7fa704a9a84032a823bfe9aee75f",
+    "url": "/static/media/cncBendingCapabilities.ceea7fa7.JPG"
+  },
+  {
+    "revision": "6edc678c969bbe362b59b5d5e6c01847",
+    "url": "/static/media/cncMachiningCapabilities.6edc678c.JPG"
+  },
+  {
+    "revision": "477b8fbf4aaf5f098c6e07222ee71847",
+    "url": "/static/media/construction722x440.477b8fbf.jpg"
+  },
+  {
+    "revision": "8ec8640f5c0078b85417d64477171a32",
+    "url": "/static/media/controlPanelCapabilities.8ec8640f.JPG"
+  },
+  {
+    "revision": "ca3ffa685549e9edbb0c3a0404f6416f",
+    "url": "/static/media/customEncloserCapabilities.ca3ffa68.JPG"
+  },
+  {
+    "revision": "d7bb31695251054b1d5d62d1bf529dee",
+    "url": "/static/media/customEnclosures300x225.d7bb3169.JPG"
+  },
+  {
+    "revision": "10f1c7876c5dcfd207fbe05aadff05d8",
+    "url": "/static/media/customFab1-544x408.10f1c787.JPG"
+  },
+  {
+    "revision": "6ff5304a0fa0ff8e680b23ae87fbef3f",
+    "url": "/static/media/customFab2-544x408.6ff5304a.JPG"
+  },
+  {
+    "revision": "1986a6d8cab6309e16f6f1d50d549a77",
+    "url": "/static/media/customFab3-544x408.1986a6d8.JPG"
+  },
+  {
+    "revision": "492d8a2507ee33cadf28d193aef7bab2",
+    "url": "/static/media/customFab5-544x408.492d8a25.JPG"
+  },
+  {
+    "revision": "5c493c3fcad9bcfda2fde78b59ced46b",
+    "url": "/static/media/customFab6-544x408.5c493c3f.JPG"
+  },
+  {
+    "revision": "8b98c17eafc2c91aa82fc166868fcc08",
+    "url": "/static/media/customFab7-544x408.8b98c17e.JPG"
+  },
+  {
+    "revision": "34ac204524f65f031ca4c9fd74643316",
+    "url": "/static/media/customFabCapabilities.34ac2045.JPG"
+  },
+  {
+    "revision": "ef6dea41cad83b8b8a88cabe529a8b15",
+    "url": "/static/media/customFabrication300x225.ef6dea41.JPG"
+  },
+  {
+    "revision": "7d91755166fd557af35e3dc8a36dad08",
+    "url": "/static/media/dark-paths.7d917551.png"
+  },
+  {
+    "revision": "6e2fcd30b57de5c390bbd0856f855629",
+    "url": "/static/media/daviRoll2_300x203.6e2fcd30.jpg"
+  },
+  {
+    "revision": "0b2b74066f069e1a2bc5b5d68470513b",
+    "url": "/static/media/designPlans300x225.0b2b7406.jpg"
+  },
+  {
+    "revision": "eaa803c0a1e77161239cf6d9c0515521",
+    "url": "/static/media/email_1.eaa803c0.png"
+  },
+  {
+    "revision": "09255bdc173de37afa94e072e3b788d0",
+    "url": "/static/media/facebook.09255bdc.png"
+  },
+  {
+    "revision": "d1fd4f083859e37c625f27d9d8833c20",
+    "url": "/static/media/footerBackground1.d1fd4f08.JPG"
+  },
+  {
+    "revision": "ffb40d7c7e206ee62284aed378767ff9",
+    "url": "/static/media/haasMill300x199.ffb40d7c.jpg"
+  },
+  {
+    "revision": "cbd9e6e3df342365b39d17592b30bab9",
+    "url": "/static/media/industryConstruction.cbd9e6e3.jpg"
+  },
+  {
+    "revision": "b3378f57aefdd89bb36d4144fd6be73c",
+    "url": "/static/media/industryCooling.b3378f57.jpg"
+  },
+  {
+    "revision": "115185d7845d4896828b55e4cdb720fa",
+    "url": "/static/media/industryElectric.115185d7.jpg"
+  },
+  {
+    "revision": "72fbe7d06bb6f52cb6bc4bde422c3846",
+    "url": "/static/media/laserBystronic544x408.72fbe7d0.JPG"
+  },
+  {
+    "revision": "928cc87114ab1861aecb563ed5460df1",
+    "url": "/static/media/laserCuttingCapabilities.928cc871.JPG"
+  },
+  {
+    "revision": "eae7dc93b5d9ed1d1e36395791d16ab8",
+    "url": "/static/media/laser_1262x841_text1.eae7dc93.jpg"
+  },
+  {
+    "revision": "66b8c8acfea8323955e13b44ea1572e2",
+    "url": "/static/media/laser_2_1262x841_text1.66b8c8ac.jpg"
+  },
+  {
+    "revision": "c529b615d6ef6e41f6271f3d0d8e88de",
+    "url": "/static/media/linkedin.c529b615.png"
+  },
+  {
+    "revision": "08089f4ee268c88631cc5da4e578d9bf",
+    "url": "/static/media/murataColorBlue.08089f4e.JPG"
+  },
+  {
+    "revision": "595649b2f64825f88617c4b288dffccb",
+    "url": "/static/media/phone_1.595649b2.png"
+  },
+  {
+    "revision": "0efd08627cb3fe363c948c94149eefae",
+    "url": "/static/media/plant_floor_2_400x300.0efd0862.JPG"
+  },
+  {
+    "revision": "a1ac5adc3d083a7cf8a41ff635d74018",
+    "url": "/static/media/powderCoat1-544x408.a1ac5adc.JPG"
+  },
+  {
+    "revision": "75aaa1b8532e4b41327e2ef33d057571",
+    "url": "/static/media/powderCoat300x200.75aaa1b8.jpg"
+  },
+  {
+    "revision": "6b948d575a827a870ddbe7b363da7c83",
+    "url": "/static/media/powderCoatingCapabilities.6b948d57.JPG"
+  },
+  {
+    "revision": "791bc38a5707f8e1542bbf0cef7280f9",
+    "url": "/static/media/pressBrake1-544x408.791bc38a.JPG"
+  },
+  {
+    "revision": "528394839f207283af240156b947b6fb",
+    "url": "/static/media/pressBrakes300x225.52839483.JPG"
+  },
+  {
+    "revision": "ff440eb4afa1c0ac9e86fbccb8eb228a",
+    "url": "/static/media/pressBrakes544x408.ff440eb4.JPG"
+  },
+  {
+    "revision": "4cdfeee3566250b21e02ef628b5bf0a1",
+    "url": "/static/media/press_brake_1259x841_text1.4cdfeee3.jpg"
+  },
+  {
+    "revision": "344fce4501dcb268532120976e079baa",
+    "url": "/static/media/prototypeContractManu.344fce45.JPG"
+  },
+  {
+    "revision": "b174f1f28025c5f01e89d5957a3a2e88",
+    "url": "/static/media/relayPanel300x225.b174f1f2.jpg"
+  },
+  {
+    "revision": "d0fe9c4fa934aaf0770f1b560168c82a",
+    "url": "/static/media/robotWelding1-544x408.d0fe9c4f.JPG"
+  },
+  {
+    "revision": "95b75184818e431349c7147276d59ef7",
+    "url": "/static/media/robotWelding_text1.95b75184.jpg"
+  },
+  {
+    "revision": "8c0037601cb0542a65668325861685df",
+    "url": "/static/media/shopFloorNorth.8c003760.JPG"
+  },
+  {
+    "revision": "8090b9975ee1d9c9698c9695dafff7bb",
+    "url": "/static/media/thumb_trump_laser400.8090b997.JPG"
+  },
+  {
+    "revision": "ca836a81bdb548f94fa7317f9610cb6e",
+    "url": "/static/media/trumpf_laser300x225.ca836a81.JPG"
+  },
+  {
+    "revision": "be3d5dc93c48c934fc80a0512b25f4d2",
+    "url": "/static/media/trumpf_laser544x408.be3d5dc9.JPG"
+  },
+  {
+    "revision": "48f741e326a853dc46230b9a19bbbebe",
+    "url": "/static/media/turretPress300x225.48f741e3.JPG"
+  },
+  {
+    "revision": "8dd46233301526a946dd08c35db3188a",
+    "url": "/static/media/turretPunchingCapabilities.8dd46233.JPG"
+  },
+  {
+    "revision": "a6da903f8c664316924ff01a6f976cfa",
+    "url": "/static/media/typical_parts1.a6da903f.JPG"
+  },
+  {
+    "revision": "bbb87c46dba88db1bf7deb27a540902e",
+    "url": "/static/media/typical_parts10.bbb87c46.JPG"
+  },
+  {
+    "revision": "b393dc1fd287d5bf0385574266561799",
+    "url": "/static/media/typical_parts11.b393dc1f.JPG"
+  },
+  {
+    "revision": "885721b3664966ae3fe767010a3a2db5",
+    "url": "/static/media/typical_parts12.885721b3.JPG"
+  },
+  {
+    "revision": "309014a6868c62a6cff6b14c4a580a5c",
+    "url": "/static/media/typical_parts13.309014a6.JPG"
+  },
+  {
+    "revision": "81b7d092e546a102bc00657cf60aae5e",
+    "url": "/static/media/typical_parts14.81b7d092.JPG"
+  },
+  {
+    "revision": "947f7fb97200656de11fa6e49e237f05",
+    "url": "/static/media/typical_parts15.947f7fb9.JPG"
+  },
+  {
+    "revision": "81d1fc87e923db99d4bc5da19928e156",
+    "url": "/static/media/typical_parts16.81d1fc87.JPG"
+  },
+  {
+    "revision": "5bc0960730a0d85d356499ce004814b9",
+    "url": "/static/media/typical_parts17.5bc09607.JPG"
+  },
+  {
+    "revision": "524ef1356ebbb4b09933e1b7957f644a",
+    "url": "/static/media/typical_parts18.524ef135.JPG"
+  },
+  {
+    "revision": "116137b916d154dd4253573bd85e3377",
+    "url": "/static/media/typical_parts19.116137b9.JPG"
+  },
+  {
+    "revision": "3b542a1bc309d4a7086a632362d9dcd4",
+    "url": "/static/media/typical_parts2.3b542a1b.JPG"
+  },
+  {
+    "revision": "ef254cea42c7ccfdb7c45f697202a4cc",
+    "url": "/static/media/typical_parts20.ef254cea.JPG"
+  },
+  {
+    "revision": "562eae9ce286aa11b3c07f02478be44a",
+    "url": "/static/media/typical_parts21.562eae9c.JPG"
+  },
+  {
+    "revision": "4e76deda2cb8e9194d8494b8c1250422",
+    "url": "/static/media/typical_parts22.4e76deda.JPG"
+  },
+  {
+    "revision": "9efbf6bf08f9b3ccef4530a37491631c",
+    "url": "/static/media/typical_parts23.9efbf6bf.JPG"
+  },
+  {
+    "revision": "0a480e9f789e0e702e7443e16f36acfe",
+    "url": "/static/media/typical_parts24.0a480e9f.JPG"
+  },
+  {
+    "revision": "7cee0b8c2c32df679f93260d55b2f62b",
+    "url": "/static/media/typical_parts25.7cee0b8c.JPG"
+  },
+  {
+    "revision": "03cd961d00ad242fa9a38ad975e517e1",
+    "url": "/static/media/typical_parts26.03cd961d.JPG"
+  },
+  {
+    "revision": "e8b1602e8d7d937b99ea109b856797d2",
+    "url": "/static/media/typical_parts27.e8b1602e.JPG"
+  },
+  {
+    "revision": "34a4a4c936b33940e7c176d6b3ae60d7",
+    "url": "/static/media/typical_parts28.34a4a4c9.JPG"
+  },
+  {
+    "revision": "511727e58d0388ff80b74fd1186f2bf2",
+    "url": "/static/media/typical_parts29.511727e5.jpg"
+  },
+  {
+    "revision": "e160a3bbf658c426545cf07744a83258",
+    "url": "/static/media/typical_parts3.e160a3bb.JPG"
+  },
+  {
+    "revision": "c2a99869b1cc2dcaa2db382a33e95364",
+    "url": "/static/media/typical_parts30.c2a99869.JPG"
+  },
+  {
+    "revision": "767b398e413d171286fd03182221852f",
+    "url": "/static/media/typical_parts31.767b398e.JPG"
+  },
+  {
+    "revision": "5423f7ffb6a1f1b7cddb66132f687302",
+    "url": "/static/media/typical_parts32.5423f7ff.JPG"
+  },
+  {
+    "revision": "ca8b6ed0705d6bd9425eddac5ed962de",
+    "url": "/static/media/typical_parts33.ca8b6ed0.JPG"
+  },
+  {
+    "revision": "9a3ac4a222813504dade1e9238601c69",
+    "url": "/static/media/typical_parts34.9a3ac4a2.JPG"
+  },
+  {
+    "revision": "a055674ff07f4cf9dbba51e1aef72caa",
+    "url": "/static/media/typical_parts35.a055674f.JPG"
+  },
+  {
+    "revision": "e16624bc8085e954ddc21677bde658fb",
+    "url": "/static/media/typical_parts4.e16624bc.JPG"
+  },
+  {
+    "revision": "496688bc5b030616009f21d67b70573e",
+    "url": "/static/media/typical_parts5.496688bc.JPG"
+  },
+  {
+    "revision": "1c68418d66f010f1cee136a0ad4d69f7",
+    "url": "/static/media/typical_parts6.1c68418d.JPG"
+  },
+  {
+    "revision": "ace07c7e40ab610b42d9b174fa21127d",
+    "url": "/static/media/typical_parts7.ace07c7e.JPG"
+  },
+  {
+    "revision": "2cafb8e834c617eeffddfb05b77f750c",
+    "url": "/static/media/typical_parts8.2cafb8e8.JPG"
+  },
+  {
+    "revision": "62dd353224f440ed2b9260e1fe662f7d",
+    "url": "/static/media/typical_parts9.62dd3532.JPG"
+  },
+  {
+    "revision": "781f0f53bf29dda5a25432261c0c9564",
+    "url": "/static/media/utilityElectrical722x481.781f0f53.jpg"
+  },
+  {
+    "revision": "76777019f6d966359efc52df553cb8cf",
+    "url": "/static/media/welding2-544x408.76777019.JPG"
+  },
+  {
+    "revision": "627c0d79013d04338f7f5cad6e039ebc",
+    "url": "/static/media/welding300x192.627c0d79.jpg"
+  },
+  {
+    "revision": "7ee01240e2b689d1138f50f56fbbc165",
+    "url": "/static/media/weldingCapabilities.7ee01240.JPG"
+  },
+  {
+    "revision": "9b3fc57143206ec073e584f95a8b0b01",
+    "url": "/static/media/wireAssembly300x225.9b3fc571.JPG"
+  }
+]);
